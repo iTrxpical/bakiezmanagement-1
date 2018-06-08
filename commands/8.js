67ce7f42563fe03a0ba1a7e872199e5e380a59c1
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-07 22:06:49 -0500

    Update 8.js

--- commands/8.js
+++ commands/8.js
@@ -1,4 +1,4 @@
-exports.run = (Discord, client, message, args, footer, randomColor) => {
+exports.run = (Discord, client, message, args, footer) => {
 var fortunes = [
     "`As I see it, yes`",
     "`It is certain`",
@@ -11,12 +11,6 @@ var fortunes = [
     "`As far as I can see, no`",
     "`I am currently unsure, please try again`"
 ] 
-var ballembed =  new Discord.RichEmbed()
-
-if (args[1]) then
-.addField("fortunes[Math.floor(Math.random() * fortunes.length");
-.setFooter(footer)
-.setColor(randomColor)
-if (!args[1]) then; message.channel.send("I do not understand your request.");
-message.channel.send(ballembed);
+if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+else message.channel.send("You did not enter a valid question.");
 }
