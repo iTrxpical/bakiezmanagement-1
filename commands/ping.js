f0fc7f243f1938b3b95296b005f2837a01427da4
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-07 21:56:49 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,5 @@
-exports.run = (Discord, client, message, args, footer) => { //This is what ever commandFile.run(Discord, client, message, args); had in the brackets. This means we don't have to keep defining the same thing.
-
-var randomColor = Math.floor(Math.random() * 16777215).toString(16); //Just gets a random color. Feel free to not put this, I just like it lol.
+exports.run = (Discord, client, message, args, footer, randomColor) => { //This is what ever commandFile.run(Discord, client, message, args); had in the brackets. This means we don't have to keep defining the same thing.
+ //Just gets a random color. Feel free to not put this, I just like it lol.
 
 var embedping = new Discord.RichEmbed() // Makes the embed so we can send it.
 
