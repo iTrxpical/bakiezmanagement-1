ef31958ef6e7c088d07fba75d1dac8ad0b570a11
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-07 21:56:37 -0500

    Update spamme.js

--- commands/spamme.js
+++ commands/spamme.js
@@ -1,9 +1,8 @@
-exports.run = (Discord, client, message, args, footer) => {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+exports.run = (Discord, client, message, args, footer, randomColor) => {
     var embedspam = new Discord.RichEmbed()
-    if (message.channel.type === 'dm') {   
+    if (message.channel.type === 'dm') then   
     .setColor(randomColor) //If you don't do random color, put a hex code in inverted commas with a # infront.
     .addField("You requested spam, here you go!") //Makes the field.
     .setFooter(footer)
     message.author.send(embedspam);
-    }}
+    }
