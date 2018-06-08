da259c7f06d32d61f2c7d41545b636c2856ade8c
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-06-08 19:35:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ client.on('message', message => {
         try {
 
             let commandFile = require(`./commands/${cmd}.js`);
-            commandFile.run(Discord, client, message, args, footer);
+            commandFile.run(Discord, client, message, args, footer, randomcolor);
 
         } catch (e) {
 
