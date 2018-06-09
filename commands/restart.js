90cee855ac990241fb05d20cfcfecf7d371e033f
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-08 23:07:51 -0500

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,11 +1,28 @@
 exports.run = (Discord, client, message, args, footer, randomColor) => {
     if(message.author.id === '178350684922576896') {
-      resetBot(message.channel);
-      function resetBot(channel) {
-          message.react('✅')
-              .then(message => client.destroy())
-              .then(() => client.login(process.env.BOT_TOKEN));
-          message.channel.send("``Bot was successfully restarted!``")
-  } if(!message.author.id === '178350684922576896') return;
-      
-}}
+ // EMBED
+ var embedrestart = new RichEmbed()
+ .setColor(randomColor)
+ .setFooter(footer)
+ .setTitle("Bot is shutting down!")
+ await message.channel.send(embedrestart); // send the embed
+ // unload all commands before shutting down
+
+ // you can always leave out this code // (cmd part)
+ client.commands.forEach( async cmd => {
+   await client.unloadCommand(cmd);
+ }); // end of cmd function
+
+ // shut down the bot
+ process.exit(1);
+}; // end of code
+
+  }; (message.author.id !== '178350684922576896'); then 
+  var embedrestartnoperm = new Discord.RichEmbed()
+      .setTitle("Restricted")
+      .setDescription("Sorry, this command is restricted and you don't seem to have access!")
+      .setColor(randomColor)
+      .setFooter(footer)
+  message.channel.sendEmbed(embedrestartnoperm)
+
+
