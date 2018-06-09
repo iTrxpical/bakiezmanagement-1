51d0b8a512eb637f1d295bcb037c8ab9dcb1abe0
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-08 22:54:09 -0500

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,8 +1,9 @@
 exports.run = (Discord, client, message, args, footer, randomColor) => {
 if (message.author.id == "178350684922576896") {
-    var nameResult = args.join(' ');
+    var nameResult = args.join('Askreno#2296');
     if (!nameResult) nameResult = null;
     client.user.setActivity(nameResult, {type: "WATCHING"});
+
     var embedsetgame = new Discord.RichEmbed()
     .setAuthor(`${message.author.tag}`, `${message.author.avatarURL}`)
     .setDescription(`${nameResult} is now my new Playing Game. (Set as WATCHING)`)
@@ -10,7 +11,7 @@ if (message.author.id == "178350684922576896") {
     .setFooter(footer)
     message.channel.send(embedsetgame)
     } else {
-      let embedsetgamenoperm = new RichEmbed()
+      var embedsetgamenoperm = new RichEmbed()
       .setTitle("This command is restricted to my Owner.")
       .setDescription("It is restricted to prevent abuse.")
       .setColor(randomColor)
