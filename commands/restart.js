985d367894cda1a17567a316bfb7c586459c07d5
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-08 22:58:38 -0500

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,13 +1,11 @@
 exports.run = (Discord, client, message, args, footer, randomColor) => {
     if(message.author.id === '178350684922576896') {
-      message.channel.send('Restarted!');
+      resetBot(message.channel);
+      function resetBot(channel) {
+          message.react('✅')
+              .then(message => client.destroy())
+              .then(() => client.login(process.env.BOT_TOKEN));
+          message.channel.send("``Bot was successfully restarted!``")
+  } if(!message.author.id === '178350684922576896') return;
       
-      setInterval(function(){ process.exit(); }, 3000);
-      
-    
-
-  } else {
-      
-      return;
-      
-}}   
+}}
